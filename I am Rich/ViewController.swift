7ea0611c3bad87bec0e5d55fe9d2bb42601bb91c
Jeremy Kim <jedo@alumni.unc.edu>
2025-02-20 20:55:59 -0500

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Jeremy Kim on 2/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
